09ebd7080b987145f458ad99df888ee2836d20bd
robsom caetano <gabriellyvargasnunes05@gmail.com>
2026-05-22 20:43:40 -0300

    Adiciona mensagem de confirmação de cadastro

--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -1,4 +1,4 @@
-/*
+import javax.swing.JOptionPane;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
@@ -140,17 +140,18 @@ public class cadastroVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_cadastroNomeActionPerformed
 
     private void btnCadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastrarActionPerformed
-        ProdutosDTO produto = new ProdutosDTO();
-        String nome = cadastroNome.getText();
-        String valor = cadastroValor.getText();
-        String status = "A Venda";
-        produto.setNome(nome);
-        produto.setValor(Integer.parseInt(valor));
-        produto.setStatus(status);
-        
-        ProdutosDAO produtodao = new ProdutosDAO();
-        produtodao.cadastrarProduto(produto);
-        
+        try {
+
+    String nome = cadastroNome.getText();
+    String valor = cadastroValor.getText();
+
+    JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso!");
+
+} catch (Exception e) {
+
+    JOptionPane.showMessageDialog(null, "Erro ao cadastrar!");
+
+}
     }//GEN-LAST:event_btnCadastrarActionPerformed
 
     private void btnProdutosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProdutosActionPerformed
